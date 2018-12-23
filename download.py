7d527ff701677c4a22133209c7cd7be09d77c037
Xplore-Labs <44762761+Xplore-Labs@users.noreply.github.com>
2018-12-23 23:32:06 +0530

    Update download.py

--- download.py
+++ download.py
@@ -1,22 +1,29 @@
-from imageai.Detection import ObjectDetection
 import dropbox
+from imageai.Detection import ObjectDetection
 import os
-dbx=dropbox.Dropbox('RsX-jbaVX9AAAAAAAAAAHHIzoAHauC0FAO_BVFO3K4efN90u2uBs_GzbXB4NbvzV')
+global i
+global j
+i=0
+j=0
 execution_path = os.getcwd()
 detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
-detector.setModelPath( os.path.join(execution_path , "resnet50_weights_tf_dim_ordering_tf_kernels.h5"))
+detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
 detector.loadModel()
-
 while 1:
-    for i in range(100):
+     try:
         x=str(i)+".jpg"
         file_name=x
         dropbox_path='/drone/'
-        with open(file_name, 'rb') as f:
-            dbx.files_download(f.read(),dropbox_path+file_name,mute=True)
-            detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "{0}.jpg".format(i)), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
-
-            for eachObject in detections:
-                print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+        dbx=dropbox.Dropbox('RsX-jbaVX9AAAAAAAAAAHHIzoAHauC0FAO_BVFO3K4efN90u2uBs_GzbXB4NbvzV')
+        dbx.files_download_to_file(str(x),dropbox_path+file_name)
         print("Downloaded %s"%(x))
+        detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path ,x), output_image_path=os.path.join(r'C:\Users\Rishi\Desktop\drones\output' , "{0}.jpg".format(i)))
+        for eachObject in detections:
+              print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
+              if eachObject["name"]=="person":
+                  j=j+1              
+        print("There are totally " + str(j) + " people in the locality" )
+        i=i+1
+     except dropbox.exceptions.ApiError:
+        print("Error in downloading %s"%(i))
